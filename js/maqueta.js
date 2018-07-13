68ad62e461b06d6e38fa9629c58215005c35518e
drakezair <luixdxd@gmail.com>
2018-07-13 14:06:59 -0400

    Modals ingresa 1:
    *Asset añadidos
    *algunos modas añidos
    
    nota:
    //faltan dos modals por añadir
    //Faltan iconos formulario
    //Faltan iconos formularios

--- js/maqueta.js
+++ js/maqueta.js
@@ -1,8 +1,12 @@
 var $body = document.getElementById('body');
-var $dimmer = document.getElementById('dimmer');
+var $dimmerNacionalidad = document.getElementById('dimmerNacionalidad');
+var $dimmerSigIn = document.getElementById('dimmerSigIn');
 var $modal = document.getElementById('modal');
-var $nacionalidad = document.getElementById('nacionalidadModal')
-var $idioma = document.getElementById('idiomaModal')
+var $nacionalidad = document.getElementById('nacionalidadModal');
+var $idioma = document.getElementById('idiomaModal');
+var $ingresa = document.getElementById('ingresaModal');
+var $signIn = document.getElementById('signInModal');
+var $signInManual = document.getElementById('signInManual');
 
 function closeDimmer() {
   $modal.classList.add('closeAnim');
@@ -10,7 +14,7 @@ function closeDimmer() {
     if(e.animationName=== 'closeModal'){
 
       $body.classList.toggle('bodyDimmer');
-      $dimmer.classList.toggle('dimmerActive');
+      $dimmerNacionalidad.classList.toggle('dimmerActive');
     }
   })
 }
@@ -28,10 +32,65 @@ function changeModal (from, to) {
     if($from !== $nacionalidad){
       $nacionalidad.classList.remove('modalsActive')
     }
+    if($from !== $signIn){
+       $signIn.classList.remove('modalsActive')
+    }
+    if($from !== $ingresa){
+       $ingresa.classList.remove('modalsActive')
+    }
+    if($from !== $signInManual){
+       $signInManual.classList.remove('modalsActive')
+    }
     if(event.animationName==='closeModal'){
         $from.classList.remove('closeAnim');
         $from.classList.remove('modalsActive')
         $to.classList.add('modalsActive');
       }
   })
+}
+
+function changeModalIngresa(from, to){
+  let $from = document.getElementById(from);
+  let $to = document.getElementById(to)
+  //
+
+  $from.classList.add('closeAnim');
+  $from.addEventListener('animationend',(event)=>{
+    if($from !== $signIn){
+       $signIn.classList.remove('modalsActive')
+    }
+    if($from !== $ingresa){
+       $ingresa.classList.remove('modalsActive')
+    }
+    if($from !== $signInManual){
+       $signInManual.classList.remove('modalsActive')
+    }
+    if(event.animationName==='closeModal'){
+        $from.classList.remove('closeAnim');
+        $from.classList.remove('modalsActive')
+        $to.classList.add('modalsActive');
+      }
+  })
+}
+
+
+function openSignIn ()
+{
+  $ingresa.classList.add('modalsActive');
+  $signIn.classList.remove('modalsActive');
+  $signInManual.classList.remove('modalsActive');
+  $dimmerSigIn.classList.add('dimmerActive');
+  $body.classList.add('bodyDimmer');
+}
+
+function closeSignIn(current)
+{
+    var $current = document.getElementById(current);
+    $current.classList.add('closeAnim');
+    setTimeout(()=>{
+      $body.classList.remove('bodyDimmer');
+      $dimmerSigIn.classList.remove('dimmerActive')
+      $current.classList.remove('closeAnim');
+    },0300);
+    console.log('HOLA');
 }
